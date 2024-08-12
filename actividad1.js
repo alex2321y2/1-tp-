d4f67471ae7678c268d525c3da24fc11f57658e2
alex2321y2 <aramayojair@gmail.com>
2024-08-12 16:39:50 -0300

    entregado

---
+++ actividad1.js
@@ -0,0 +1,21 @@
+/*cargamosun arreglo medianteelprompt cuando trabajamos con texto y con promp + parseint cuando cargamos numeros decimales trabajamos con el prompt + parsefloat  */
+var arreglovacio[]
+var n=5
+for (let i=0;i<n; i++){
+arreglovacio[i]=parseFloat(prompt("ingrese sueldo:"));
+arreglovacio[i]=parseInt(prompt("ingrese una cantidad de alumnos"));
+arreglovacio[i]prompt("ingrese nombre");
+arreglovacio[i]Math.floor(math.random()*10)
+}
+document.write(arreglovacio.join(""))
+
+var arregloalumnos=["ana","laura","santiago","luis"]
+document.write(arregloalumnos.join(""))
+/*metodos para agregar elemnetos:
+unshift:agregamos elementos al principio
+push: agregamos elemnetos al final   */
+arregloalumnos.unshift("franco")
+document.write(arregloalumnos.join(""))
+document.write("agregamos mediante el metodo push:"+"<br>")
+arregloalumnos.push("lucas")
+document.write(arregloalumnos.join(""))
